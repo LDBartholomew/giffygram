0debd3a3db3c3670a64605505f079845799e4922
LD Bartholomew <LDBartholomew@yahoo.com>
2021-09-22 11:28:10 -0500

    added delete, edit, like buttons

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,13 +1,19 @@
 // Can you explain what is being imported here?
-import { getPosts, getUsers, usePostCollection, createPost } from "./data/DataManager.js"
+import { getPosts, getUsers, usePostCollection, 
+	createPost, deletePost, getSinglePost, updatePost, 
+	getLoggedInUser, logoutUser, setLoggedInUser, loginUser, registerUser, postLike } from "./data/DataManager.js"
 import { PostList } from "./feed/PostList.js"
 import { NavBar } from "./nav/NavBar.js"
 import { Footer } from "./nav/Footer.js";
 import { PostEntry } from "./feed/PostEntry.js";
+import { PostEdit } from "./feed/PostEdit.js";
+import { LoginForm } from "./auth/LoginForm.js";
+import { RegisterForm } from "./auth/RegisterForm.js"
+
 
 let yearSelected = 2021; //default
 
-
+const applicationElement = document.querySelector(".giffygram")
 
 const showPostList = () => {
 	//Get a reference to the location on the DOM where the list will display
@@ -29,12 +35,33 @@ const showPostEntry = () => {
 	entryElement.innerHTML = PostEntry();
 }
 
+const checkForUser = () => {
+	if (sessionStorage.getItem("user")){
+	  //this is expecting an object. Need to fix
+		setLoggedInUser(JSON.parse(sessionStorage.getItem("user")));
+	  startGiffyGram();
+	}else {
+		showLoginRegister();
+	 // console.log("showLogin")
+	}
+  }
+  
+  const showLoginRegister = () => {
+	showNavBar();
+	const entryElement = document.querySelector(".entryForm");
+	//template strings can be used here too
+	entryElement.innerHTML = `${LoginForm()} <hr/> <hr/> ${RegisterForm()}`;
+	//make sure the post list is cleared out too
+  const postElement = document.querySelector(".postList");
+  postElement.innerHTML = "";
+}
+
 const showFooter = (yearSelected) => {
 	const footerElement = document.querySelector("footer");
 	footerElement.innerHTML = Footer(yearSelected)
   }
 
-const applicationElement = document.querySelector(".giffygram")
+
 ;
 
 applicationElement.addEventListener("click", event => {
@@ -64,6 +91,9 @@ applicationElement.addEventListener("change", event => {
 	const postElement = document.querySelector(".postList");
 	postElement.innerHTML = PostList(filteredData);
 }
+
+
+
  const handleGiffyClick = (event) => {
       console.log("what was clicked", event)
 	  if (event.target.id === "logout"){
@@ -113,6 +143,121 @@ applicationElement.addEventListener("click", (event) => {
 
  applicationElement.addEventListener("click", handleGiffyClick)
 
+ applicationElement.addEventListener("click", event => {
+	event.preventDefault();
+	if (event.target.id.startsWith("delete")) {
+	  const postId = event.target.id.split("__")[1];
+	  deletePost(postId)
+		.then(response => {
+		  showPostList();
+		})
+	}
+  })
+  
+  applicationElement.addEventListener("click", event => {
+	event.preventDefault();
+	if (event.target.id.startsWith("edit")) {
+	  const postId = event.target.id.split("__")[1];
+	  getSinglePost(postId)
+		.then(response => {
+		  showEdit(response);
+		})
+	}
+  })
+  
+  const showEdit = (postObj) => {
+	const entryElement = document.querySelector(".entryForm");
+	entryElement.innerHTML = PostEdit(postObj);
+  }
+
+  applicationElement.addEventListener("click", event => {
+	event.preventDefault();
+	if (event.target.id.startsWith("updatePost")) {
+	  const postId = event.target.id.split("__")[1];
+	  //collect all the details into an object
+	  const title = document.querySelector("input[name='postTitle']").value
+	  const url = document.querySelector("input[name='postURL']").value
+	  const description = document.querySelector("textarea[name='postDescription']").value
+	  const timestamp = document.querySelector("input[name='postTime']").value
+	  
+	  const postObject = {
+		title: title,
+		imageURL: url,
+		description: description,
+		userId: getLoggedInUser().id,
+		timestamp: parseInt(timestamp),
+		id: parseInt(postId)
+	  }
+	  
+	  updatePost(postObject)
+		.then(response => {
+		  showPostList();
+		})
+	}
+  })
+  
+  applicationElement.addEventListener("click", event => {
+	event.preventDefault();
+	if (event.target.id.startsWith("like")) {
+	  const likeObject = {
+		 postId: parseInt(event.target.id.split("__")[1]),
+		 userId: getLoggedInUser().id
+	  }
+	  postLike(likeObject)
+		.then(response => {
+		  showPostList();
+		})
+	}
+  })
+
+  applicationElement.addEventListener("click", event => {
+	if (event.target.id === "logout") {
+	  logoutUser();
+	  console.log(getLoggedInUser());
+	  sessionStorage.clear();
+	  checkForUser();
+	}
+  })
+  applicationElement.addEventListener("click", event => {
+	event.preventDefault();
+	if (event.target.id === "login__submit") {
+	  //collect all the details into an object
+	  const userObject = {
+		name: document.querySelector("input[name='name']").value,
+		email: document.querySelector("input[name='email']").value
+	  }
+	  loginUser(userObject)
+	  .then(dbUserObj => {
+		if(dbUserObj){
+		  sessionStorage.setItem("user", JSON.stringify(dbUserObj));
+		  startGiffyGram();
+		}else {
+		  //got a false value - no user
+		  const entryElement = document.querySelector(".entryForm");
+		  entryElement.innerHTML = `<p class="center">That user does not exist. Please try again or register for your free account.</p> ${LoginForm()} <hr/> <hr/> ${RegisterForm()}`;
+		}
+	  })
+	}
+  })
+  
+  applicationElement.addEventListener("click", event => {
+	event.preventDefault();
+	if (event.target.id === "register__submit") {
+	  //collect all the details into an object
+	  const userObject = {
+		name: document.querySelector("input[name='registerName']").value,
+		email: document.querySelector("input[name='registerEmail']").value
+	  }
+	  registerUser(userObject)
+	  .then(dbUserObj => {
+		sessionStorage.setItem("user", JSON.stringify(dbUserObj));
+		startGiffyGram();
+	  })
+	}
+  })
+  
+  
+
 const startGiffyGram = () => {
     showNavBar();
 	showPostList();
@@ -120,4 +265,6 @@ const startGiffyGram = () => {
 	showFooter(yearSelected);
 }
 
-startGiffyGram();
+checkForUser();
+//startGiffyGram();
+
